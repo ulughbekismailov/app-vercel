53e83d36d068db6a18b69807154b7a032cead44a
UI <xmail@kali.xmail>
2026-02-19 17:02:48 -0600

    Product page edited

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -196,7 +196,7 @@
           <h2 class="text-lg font-semibold text-gray-900 dark:text-white mb-4">
             Quantity
           </h2>
-          <QuantitySelector v-model="quantity" :min="1" :max="99" />
+          <QuantitySelector v-if="!isInCart" v-model="quantity" :min="1" :max="99" />
         </div>
       </div>
     </div>
@@ -226,7 +226,7 @@
           <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
           </svg>
-          <span>{{ isInCart ? 'Update Cart' : 'Add to Cart' }}</span>
+          <span>{{ isInCart ? 'Go to Cart' : 'Add to Cart' }}</span>
         </button>
       </div>
     </div>
@@ -316,20 +316,7 @@ const addToCart = async (event) => {
       console.error('Xatolik:', error);
     }
   } else {
-      try {
-        await cartStore.updateQuantity(product.value.id, quantity.value);
-        telegram.hapticFeedback('success');
-        
-        setTimeout(() => {
-          router.push('/cart');
-        }, 250);
-      
-    } catch (error) {
-        isInCart.value = false;
-        telegram.hapticFeedback('error');
-        console.error('Xatolik:', error);
-    }
-  
+    router.push('/cart');
   }
 
 };
